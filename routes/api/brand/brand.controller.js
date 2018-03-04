56e7b4cce68cb96d442da67a90360091e599cecc
Yoon Jee Woo <yoonjeewoo@gmail.com>
2018-03-04 16:50:10 +0900

    브랜드 리스트 추가

--- routes/api/brand/brand.controller.js
+++ routes/api/brand/brand.controller.js
@@ -3,24 +3,26 @@ const config = require('../../../config');
 const conn = mysql.createConnection(config);
 const lineReader = require('line-reader');
 
-exports.brand = (req, res) => {
-	// lineReader.eachLine('brand.txt', function(line, last) {
-	// 	console.log(line);
-	//   	conn.query(
-	// 	  'INSERT INTO Brands(brand_name, brand_name_kor) VALUES(?, ?)',
-	// 	  [line, null],
-	// 	  (err, result) => {
-	// 		  if (err) throw err;
-	// 	  }
-	//  	)
-	//   if(last){
-	//     // or check if it's the last one
-	// 	return res.status(200).json({
-	// 		message: 'input brandlist done'
-	// 	})
-	//   }
-	// });
-};
+// exports.brand = (req, res) => {
+// 	lineReader.eachLine('brand.txt', function(line, last) {
+// 		console.log(line);
+// 		let eng = line.split('@')[0];
+// 		let kor = line.split('@')[1];
+// 	  	conn.query(
+// 		  'INSERT INTO Brands(brand_name, brand_name_kor) VALUES(?, ?)',
+// 		  [eng, kor],
+// 		  (err, result) => {
+// 			  if (err) throw err;
+// 		  }
+// 	 	)
+// 	  if(last){
+// 	    // or check if it's the last one
+// 		return res.status(200).json({
+// 			message: 'input brandlist done'
+// 		})
+// 	  }
+// 	});
+// };
 
 exports.getBrandList = (req, res) => {
 	conn.query(
--- routes/api/brand/index.js
+++ routes/api/brand/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const controller = require('./brand.controller');
 
-router.post('', controller.brand);
+// router.post('', controller.brand);
 router.get('', controller.getBrandList);
 router.get('/one/:brand_id', controller.getOneBrand);
 
