e7e4154eb06895d2eb56ae318ede848e3adb22a9
Yoon Jee Woo <yoonjeewoo@gmail.com>
2018-03-16 10:39:55 +0900

    item recent

--- routes/api/item/item.controller.js
+++ routes/api/item/item.controller.js
@@ -248,7 +248,7 @@ exports.getPictureList = (req, res) => {
 
 exports.getRecentItems = (req, res) => {
 	conn.query(
-		'SELECT * FROM Items ORDER BY time DESC LIMIT 10',
+		'SELECT * FROM Items join Photos on Photos.item_id = Items.id WHERE first = 1 ORDER BY time DESC LIMIT 10',
 		(err, result) => {
 			if (err) throw err;
 			return res.status(200).json({
