caf5fe0eea10506ee49bee2fce93d62986c86d35
Yoon Jee Woo <yoonjeewoo@gmail.com>
2018-03-24 15:43:35 +0900

    orderby

--- routes/api/search/index.js
+++ routes/api/search/index.js
@@ -3,13 +3,4 @@ const controller = require('./search.controller');
 
 router.post('', controller.search);
 
-router.post('/price/ascend', controller.searchByPriceA);
-router.post('/price/descend', controller.searchByPriceD);
-
-router.post('/date/ascend', controller.searchByDateA);
-router.post('/date/descend', controller.searchByDateD);
-
-// router.post('', controller.searchByPriceA);
-// router.post('', controller.searchByPriceD);
-
 module.exports = router;
--- routes/api/search/search.controller.js
+++ routes/api/search/search.controller.js
@@ -5,9 +5,17 @@ const conn = mysql.createConnection(config);
 
 exports.search = (req, res) => {
 
-    const { brand_id, category_1, category_2, item_status, season, max_price, min_price, index } = req.body;
+    const { brand_id, category_1, category_2, item_status, season, max_price, min_price, index, condition } = req.body;
     let sql = "SELECT * FROM Items join Photos on (Items.id = Photos.item_id and Photos.first = 1 ) WHERE ";
-
+    if (condition == 0) {
+        condition_str = "Items.price ASC"
+    } else if (condition == 1) {
+        condition_str = "Items.price DESC"
+    } else if (condition == 2) {
+        condition_str = "Items.time ASC"
+    } else if (condition == 3) {
+        condition_str = "Items.time DESC"
+    }
     if (category_1 == '상관없음') {
         sql += 'category_1 is not null and '
     } else if (category_1 != '상관없음') {
@@ -33,7 +41,7 @@ exports.search = (req, res) => {
     } else if (brand_id != '상관없음') {
         sql += `brand_id = '${brand_id}' and `
     }
-    sql += `price >= ${min_price} and price <= ${max_price} ORDER BY Items.id DESC LIMIT 10 OFFSET ${index};`
+    sql += `price >= ${min_price} and price <= ${max_price} ORDER BY ${condition_str} LIMIT 10 OFFSET ${index};`
     conn.query(
         sql,
         (err, result) => {
@@ -45,176 +53,3 @@ exports.search = (req, res) => {
         }
     )
 }
-
-exports.searchByPriceA = (req, res) => {
-
-    const { brand_id, category_1, category_2, item_status, season, max_price, min_price, index } = req.body;
-    let sql = "SELECT * FROM Items join Photos on (Items.id = Photos.item_id and Photos.first = 1 ) WHERE ";
-
-    if (category_1 == '상관없음') {
-        sql += 'category_1 is not null and '
-    } else if (category_1 != '상관없음') {
-        sql += `category_1 = '${category_1}' and `
-    }
-    if (category_2 == '상관없음') {
-        sql += 'category_2 is not null and '
-    } else if (category_2 != '상관없음') {
-        sql += `category_2 = '${category_2}' and `
-    }
-    if (item_status == '상관없음') {
-        sql += 'item_status is not null and '
-    } else if (item_status != '상관없음') {
-        sql += `item_status = '${item_status}' and `
-    }
-    if (season == '상관없음') {
-        sql += 'season is not null and '
-    } else if (season != '상관없음') {
-        sql += `season = '${season}' and `
-    }
-    if (brand_id == '상관없음') {
-        sql += 'brand_id is not null and '
-    } else if (brand_id != '상관없음') {
-        sql += `brand_id = '${brand_id}' and `
-    }
-    sql += `price >= ${min_price} and price <= ${max_price} ORDER BY Items.price ASC LIMIT 10 OFFSET ${index};`
-    conn.query(
-        sql,
-        (err, result) => {
-            if (err) throw err;
-            return res.status(200).json({
-                nextIndex: parseInt(index) + 10,
-                result
-            })
-        }
-    )
-}
-
-exports.searchByPriceD = (req, res) => {
-
-    const { brand_id, category_1, category_2, item_status, season, max_price, min_price, index } = req.body;
-    let sql = "SELECT * FROM Items join Photos on (Items.id = Photos.item_id and Photos.first = 1 ) WHERE ";
-
-    if (category_1 == '상관없음') {
-        sql += 'category_1 is not null and '
-    } else if (category_1 != '상관없음') {
-        sql += `category_1 = '${category_1}' and `
-    }
-    if (category_2 == '상관없음') {
-        sql += 'category_2 is not null and '
-    } else if (category_2 != '상관없음') {
-        sql += `category_2 = '${category_2}' and `
-    }
-    if (item_status == '상관없음') {
-        sql += 'item_status is not null and '
-    } else if (item_status != '상관없음') {
-        sql += `item_status = '${item_status}' and `
-    }
-    if (season == '상관없음') {
-        sql += 'season is not null and '
-    } else if (season != '상관없음') {
-        sql += `season = '${season}' and `
-    }
-    if (brand_id == '상관없음') {
-        sql += 'brand_id is not null and '
-    } else if (brand_id != '상관없음') {
-        sql += `brand_id = '${brand_id}' and `
-    }
-    sql += `price >= ${min_price} and price <= ${max_price} ORDER BY Items.price DESC LIMIT 10 OFFSET ${index};`
-    conn.query(
-        sql,
-        (err, result) => {
-            if (err) throw err;
-            return res.status(200).json({
-                nextIndex: parseInt(index) + 10,
-                result
-            })
-        }
-    )
-}
-
-exports.searchByDateA = (req, res) => {
-
-    const { brand_id, category_1, category_2, item_status, season, max_price, min_price, index } = req.body;
-    let sql = "SELECT * FROM Items join Photos on (Items.id = Photos.item_id and Photos.first = 1 ) WHERE ";
-
-    if (category_1 == '상관없음') {
-        sql += 'category_1 is not null and '
-    } else if (category_1 != '상관없음') {
-        sql += `category_1 = '${category_1}' and `
-    }
-    if (category_2 == '상관없음') {
-        sql += 'category_2 is not null and '
-    } else if (category_2 != '상관없음') {
-        sql += `category_2 = '${category_2}' and `
-    }
-    if (item_status == '상관없음') {
-        sql += 'item_status is not null and '
-    } else if (item_status != '상관없음') {
-        sql += `item_status = '${item_status}' and `
-    }
-    if (season == '상관없음') {
-        sql += 'season is not null and '
-    } else if (season != '상관없음') {
-        sql += `season = '${season}' and `
-    }
-    if (brand_id == '상관없음') {
-        sql += 'brand_id is not null and '
-    } else if (brand_id != '상관없음') {
-        sql += `brand_id = '${brand_id}' and `
-    }
-    sql += `price >= ${min_price} and price <= ${max_price} ORDER BY Items.time ASC LIMIT 10 OFFSET ${index};`
-    conn.query(
-        sql,
-        (err, result) => {
-            if (err) throw err;
-            return res.status(200).json({
-                nextIndex: parseInt(index) + 10,
-                result
-            })
-        }
-    )
-}
-
-exports.searchByDateD = (req, res) => {
-
-    const { brand_id, category_1, category_2, item_status, season, max_price, min_price, index } = req.body;
-    let sql = "SELECT * FROM Items join Photos on (Items.id = Photos.item_id and Photos.first = 1 ) WHERE ";
-
-    if (category_1 == '상관없음') {
-        sql += 'category_1 is not null and '
-    } else if (category_1 != '상관없음') {
-        sql += `category_1 = '${category_1}' and `
-    }
-    if (category_2 == '상관없음') {
-        sql += 'category_2 is not null and '
-    } else if (category_2 != '상관없음') {
-        sql += `category_2 = '${category_2}' and `
-    }
-    if (item_status == '상관없음') {
-        sql += 'item_status is not null and '
-    } else if (item_status != '상관없음') {
-        sql += `item_status = '${item_status}' and `
-    }
-    if (season == '상관없음') {
-        sql += 'season is not null and '
-    } else if (season != '상관없음') {
-        sql += `season = '${season}' and `
-    }
-    if (brand_id == '상관없음') {
-        sql += 'brand_id is not null and '
-    } else if (brand_id != '상관없음') {
-        sql += `brand_id = '${brand_id}' and `
-    }
-    sql += `price >= ${min_price} and price <= ${max_price} ORDER BY Items.time DESC LIMIT 10 OFFSET ${index};`
-    conn.query(
-        sql,
-        (err, result) => {
-            if (err) throw err;
-            return res.status(200).json({
-                nextIndex: parseInt(index) + 10,
-                result
-            })
-        }
-    )
-}
-
