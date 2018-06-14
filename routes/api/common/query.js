678439b20875df77a199c75731a5e193d24ef1af
JeongHyunLee <nomar0107@gmail.com>
2018-06-14 22:02:58 +0900

    item all

--- routes/api/common/query.js
+++ routes/api/common/query.js
@@ -1,3 +1,13 @@
+const jwt = require('jsonwebtoken');
+const mysql = require('mysql');
+const config = require('../../../config');
+const conn = mysql.createConnection(config);
+const AWS = require('aws-sdk');
+AWS.config.region = 'ap-northeast-2';
+const s3 = new AWS.S3();
+const crypto = require("crypto");
+const query = require("../common/query");
+
 exports.getItemByItemId = (item_id) => {
     return new Promise((resolve, reject) => {
         conn.query(`SELECT * FROM Items WHERE item_id = ${item_id}`, (err, result) => {
