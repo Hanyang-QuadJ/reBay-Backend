ecd3acf613dcf9c8850e4b3c3f3ab2fb8af13578
Yoon Jee Woo <yoonjeewoo@gmail.com>
2018-07-06 17:50:18 +0900

    temp status removed

--- app.js
+++ app.js
@@ -50,32 +50,32 @@ app.use(morgan(':status'), function(req, res, next) {
 	next();
 });
 
-var FCM = require('fcm-node');
-var serverKey = 'AAAAyS0H1u0:APA91bFX9VjAXOe6hGbGu7CvRQg_qRZzFdOjwY_qper2qVxpiY6P-LEb5KLk_Rh96r0N9iD_NVm6yAwxzIqUZ702_wDQ2RZiNzBS9XdD3Ckf1L_bPxXHERiFmeT58g4REHGPZmT0If8G'; //put your server key here
-var fcm = new FCM(serverKey);
+// var FCM = require('fcm-node');
+// var serverKey = 'AAAAyS0H1u0:APA91bFX9VjAXOe6hGbGu7CvRQg_qRZzFdOjwY_qper2qVxpiY6P-LEb5KLk_Rh96r0N9iD_NVm6yAwxzIqUZ702_wDQ2RZiNzBS9XdD3Ckf1L_bPxXHERiFmeT58g4REHGPZmT0If8G'; //put your server key here
+// var fcm = new FCM(serverKey);
 
-var message = { //this may vary according to the message type (single recipient, multicast, topic, et cetera)
-	to: 'f0MtLpxg-tk:APA91bE0FF4peHmGtPcW5bWedKGXNglbrPYRAEAVbdkdyMKXOSiV1uVqIvhXilff_tGduJozH4wLnkrJ3gONLKFI7MURHQdI8hYNoQLgpwXfj8vu4SZFtQym6sKZl-s5dk2JkyKGz5Ocmh81cLkyhVI5yJXIoIxqXQ',
-	collapse_key: 'your_collapse_key',
+// var message = { //this may vary according to the message type (single recipient, multicast, topic, et cetera)
+// 	to: 'f0MtLpxg-tk:APA91bE0FF4peHmGtPcW5bWedKGXNglbrPYRAEAVbdkdyMKXOSiV1uVqIvhXilff_tGduJozH4wLnkrJ3gONLKFI7MURHQdI8hYNoQLgpwXfj8vu4SZFtQym6sKZl-s5dk2JkyKGz5Ocmh81cLkyhVI5yJXIoIxqXQ',
+// 	collapse_key: 'your_collapse_key',
 
-	notification: {
-		title: 'reBay',
-		body: '현종아 대체 언제 끝나냐 이거'
-	},
+// 	notification: {
+// 		title: 'reBay',
+// 		body: '현종아 대체 언제 끝나냐 이거'
+// 	},
 
-	data: {  //you can send only notification or only data(or include both)
-		my_key: 'my value',
-		my_another_key: 'my another value'
-	}
-};
+// 	data: {  //you can send only notification or only data(or include both)
+// 		my_key: 'my value',
+// 		my_another_key: 'my another value'
+// 	}
+// };
 
-fcm.send(message, function (err, response) {
-	if (err) {
-		console.log("Something has gone wrong!");
-	} else {
-		console.log("Successfully sent with response: ", response);
-	}
-});
+// fcm.send(message, function (err, response) {
+// 	if (err) {
+// 		console.log("Something has gone wrong!");
+// 	} else {
+// 		console.log("Successfully sent with response: ", response);
+// 	}
+// });
 // set the secret key variable for jwt
 app.set('jwt-secret', config.secret);
 // index page, just for testing
